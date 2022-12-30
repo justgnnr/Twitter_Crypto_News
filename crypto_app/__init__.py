1e56b089d16cc6e8370f19ccec2b5b8f9187de3b
Gunnar <gunnar@gunnar-lab.lan>
2022-12-30 11:39:06 -0600

    Add templates

--- crypto_app/__init__.py
+++ crypto_app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import render_template
 
 
 def create_app(test_config=None):
@@ -25,9 +26,9 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
 
